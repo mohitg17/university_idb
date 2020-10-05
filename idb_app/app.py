6b22273ab82ed575bf3ab8a350350065f5a3226f
Silas Strawn <strawnsc@gmail.com>
2020-10-05 00:25:29 -0500

    add dummy major instance page

--- idb_app/app.py
+++ idb_app/app.py
@@ -13,5 +13,22 @@ def majors_base():
     return render_template("majors.html")
 
 
+# TODO migrate to mongoengine after phase i
+majors = {
+    "engineering",
+    "history",
+    "education",
+}
+
+
+@app.route("/major/<string:major_name>")
+def major(major_name):
+    major_normalized = major_name.lower()
+    if major_normalized not in majors:
+        return f"Could not fine major {major_name}"
+    else:
+        return "yolo"
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
