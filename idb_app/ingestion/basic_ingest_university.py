84501bd36df129a76b57dcc152e755da6fcfa4d0
mohitg17 <mohit.gupta2563@gmail.com>
2020-10-25 00:21:22 -0500

    account for incomplete data, get all uni data

--- idb_app/ingestion/basic_ingest_university.py
+++ idb_app/ingestion/basic_ingest_university.py
@@ -18,20 +18,23 @@ def create_majors():
         mjr.save()
 
 def create_city(data):
-    city_params = {
-        'name': data['school_city'],
-        'state': data['school_state'],
-        'area': 1000000,
-        'population': 1000000,
-        'population_density': 10000,
-        'community_type': data['school_locale'],
-        'median_age': 20,
-        'median_gross_rent': 1000
-    }
+    if City.objects(name=data['school_city']):
+        return City.objects(name=data['school_city']).first()
+    else:
+        city_params = {
+            'name': data['school_city'],
+            'state': data['school_state'],
+            'area': 1000000,
+            'population': 1000000,
+            'population_density': 10000,
+            'community_type': data['school_locale'],
+            'median_age': 20,
+            'median_gross_rent': 1000
+        }
 
-    dummy_city = City(**city_params)
-    dummy_city.save()
-    return dummy_city
+        dummy_city = City(**city_params)
+        dummy_city.save()
+        return dummy_city
 
 def rename_fields(data):
     tmp = {}
@@ -45,7 +48,7 @@ def compress_majors(data):
     compressed = {}
     for key in data:
         if "latest_academics_program_bachelors" in key:
-            if data[key] > 0:
+            if data[key] and data[key] > 0:
                 majors_offered.append(Major.objects(name=key.split('_')[-1]).first())
         else:
             compressed[key] = data[key]
@@ -54,6 +57,7 @@ def compress_majors(data):
 
 def translate(cleaned):
     degrees = {
+        0: choices.UNCLASSIFIED,
         1: choices.DEGREE_CERTIFICATE,
         2: choices.DEGREE_ASSOCIATE,
         3: choices.DEGREE_BACHELORS,
@@ -61,6 +65,8 @@ def translate(cleaned):
     }
 
     locale = {
+        -2: "Unclassified",
+        -3: "Unclassified",
         11: "City",
         12: "City",
         13: "City",
@@ -79,9 +85,9 @@ def translate(cleaned):
 
     cleaned['school_degrees_awarded_highest'] = degrees[cleaned['school_degrees_awarded_highest']]
     cleaned['school_degrees_awarded_predominant'] = degrees[cleaned['school_degrees_awarded_predominant']]
-    cleaned['school_school_url'] = f"https://{cleaned['school_school_url'].split('.', 1)[1]}"
+    cleaned['school_school_url'] = f"https://{cleaned['school_school_url'].split('.', 1)[1]}" if cleaned['school_school_url'] else "None"
     cleaned['school_locale'] = locale[cleaned['school_locale']]
-    cleaned['school_state'] = states[cleaned['school_state'].upper()]
+    cleaned['school_state'] = states[cleaned['school_state'].upper()] if cleaned['school_state'].upper() in states else "Texas"
     return cleaned
 
 def query(fields:list, arguments:dict) -> dict: 
@@ -98,42 +104,45 @@ if __name__ == "__main__":
 
     create_majors()
 
-    raw_data = query(["school.name", "school.city", "school.state", "school.locale", "school.school_url", "latest.student.size", "latest.admissions.admission_rate.overall",
-    "latest.cost.tuition.in_state", "latest.cost.tuition.out_of_state", "latest.cost.attendance.academic_year", "latest.aid.median_debt.completers.overall",
-    "school.degrees_awarded.highest", "school.degrees_awarded.predominant", "latest.earnings.10_yrs_after_entry.median",
-    "latest.admissions.sat_scores.average.overall", "latest.admissions.act_scores.midpoint.cumulative",
-    
-    'latest.academics.program.bachelors.agriculture', 'latest.academics.program.bachelors.resources', 'latest.academics.program.bachelors.architecture', 
-    'latest.academics.program.bachelors.ethnic_cultural_gender', 'latest.academics.program.bachelors.communication', 
-    'latest.academics.program.bachelors.communications_technology', 'latest.academics.program.bachelors.computer', 
-    'latest.academics.program.bachelors.personal_culinary', 'latest.academics.program.bachelors.education', 
-    'latest.academics.program.bachelors.engineering', 'latest.academics.program.bachelors.engineering_technology', 
-    'latest.academics.program.bachelors.language', 'latest.academics.program.bachelors.family_consumer_science', 
-    'latest.academics.program.bachelors.legal', 'latest.academics.program.bachelors.english', 'latest.academics.program.bachelors.humanities', 
-    'latest.academics.program.bachelors.library', 'latest.academics.program.bachelors.biological', 'latest.academics.program.bachelors.mathematics', 
-    'latest.academics.program.bachelors.military', 'latest.academics.program.bachelors.multidiscipline', 
-    'latest.academics.program.bachelors.parks_recreation_fitness', 'latest.academics.program.bachelors.philosophy_religious', 
-    'latest.academics.program.bachelors.theology_religious_vocation', 'latest.academics.program.bachelors.physical_science', 
-    'latest.academics.program.bachelors.science_technology', 'latest.academics.program.bachelors.psychology', 
-    'latest.academics.program.bachelors.security_law_enforcement', 'latest.academics.program.bachelors.public_administration_social_service', 
-    'latest.academics.program.bachelors.social_science', 'latest.academics.program.bachelors.construction', 
-    'latest.academics.program.bachelors.mechanic_repair_technology', 'latest.academics.program.bachelors.precision_production', 
-    'latest.academics.program.bachelors.transportation', 'latest.academics.program.bachelors.visual_performing', 
-    'latest.academics.program.bachelors.health', 'latest.academics.program.bachelors.business_marketing', 'latest.academics.program.bachelors.history',
-    
-    "latest.student.demographics.race_ethnicity.white", "latest.student.demographics.race_ethnicity.black", "latest.student.demographics.race_ethnicity.hispanic", "latest.student.demographics.race_ethnicity.asian", "latest.student.demographics.men", "latest.student.demographics.women",
-    "latest.completion.4_yr_completion.overall", "latest.completion.completion_rate_4yr_150_white", "latest.completion.completion_rate_4yr_150_black", "latest.completion.completion_rate_4yr_150_hispanic", "latest.completion.completion_rate_4yr_150_asian", "latest.student.retention_rate.four_year.full_time"], 
-    # {"school.degrees_awarded.predominant":3}
-    {"id":228778}
-    )
+    for i in range(1,21):
+        raw_data = query(["school.name", "school.city", "school.state", "school.locale", "school.school_url", "latest.student.size", "latest.admissions.admission_rate.overall",
+        "latest.cost.tuition.in_state", "latest.cost.tuition.out_of_state", "latest.cost.attendance.academic_year", "latest.aid.median_debt.completers.overall",
+        "school.degrees_awarded.highest", "school.degrees_awarded.predominant", "latest.earnings.10_yrs_after_entry.median",
+        "latest.admissions.sat_scores.average.overall", "latest.admissions.act_scores.midpoint.cumulative",
+        
+        'latest.academics.program.bachelors.agriculture', 'latest.academics.program.bachelors.resources', 'latest.academics.program.bachelors.architecture', 
+        'latest.academics.program.bachelors.ethnic_cultural_gender', 'latest.academics.program.bachelors.communication', 
+        'latest.academics.program.bachelors.communications_technology', 'latest.academics.program.bachelors.computer', 
+        'latest.academics.program.bachelors.personal_culinary', 'latest.academics.program.bachelors.education', 
+        'latest.academics.program.bachelors.engineering', 'latest.academics.program.bachelors.engineering_technology', 
+        'latest.academics.program.bachelors.language', 'latest.academics.program.bachelors.family_consumer_science', 
+        'latest.academics.program.bachelors.legal', 'latest.academics.program.bachelors.english', 'latest.academics.program.bachelors.humanities', 
+        'latest.academics.program.bachelors.library', 'latest.academics.program.bachelors.biological', 'latest.academics.program.bachelors.mathematics', 
+        'latest.academics.program.bachelors.military', 'latest.academics.program.bachelors.multidiscipline', 
+        'latest.academics.program.bachelors.parks_recreation_fitness', 'latest.academics.program.bachelors.philosophy_religious', 
+        'latest.academics.program.bachelors.theology_religious_vocation', 'latest.academics.program.bachelors.physical_science', 
+        'latest.academics.program.bachelors.science_technology', 'latest.academics.program.bachelors.psychology', 
+        'latest.academics.program.bachelors.security_law_enforcement', 'latest.academics.program.bachelors.public_administration_social_service', 
+        'latest.academics.program.bachelors.social_science', 'latest.academics.program.bachelors.construction', 
+        'latest.academics.program.bachelors.mechanic_repair_technology', 'latest.academics.program.bachelors.precision_production', 
+        'latest.academics.program.bachelors.transportation', 'latest.academics.program.bachelors.visual_performing', 
+        'latest.academics.program.bachelors.health', 'latest.academics.program.bachelors.business_marketing', 'latest.academics.program.bachelors.history',
+        
+        "latest.student.demographics.race_ethnicity.white", "latest.student.demographics.race_ethnicity.black", "latest.student.demographics.race_ethnicity.hispanic", "latest.student.demographics.race_ethnicity.asian", "latest.student.demographics.men", "latest.student.demographics.women",
+        "latest.completion.4_yr_completion.overall", "latest.completion.completion_rate_4yr_150_white", "latest.completion.completion_rate_4yr_150_black", "latest.completion.completion_rate_4yr_150_hispanic", "latest.completion.completion_rate_4yr_150_asian", "latest.student.retention_rate.four_year.full_time"], 
+        {"per_page": 100, "page": i, "school.degrees_awarded.predominant":3}
+        # {"id":228778}
+        )
 
-    # TODO: update to ingest all university data instead of just one uni
-    uni_data = rename_fields(raw_data['results'][0])
-    uni_data = compress_majors(uni_data)
-    uni_data = translate(uni_data)
-    uni_data['school_city'] = create_city(uni_data)
+        print(raw_data['metadata'])
 
-    uni = University(**uni_data)
-    uni.save()
+        for j in range(len(raw_data['results'])):
+            uni_data = rename_fields(raw_data['results'][j])
+            uni_data = compress_majors(uni_data)
+            uni_data = translate(uni_data)
+            uni_data['school_city'] = create_city(uni_data)
+
+            uni = University(**uni_data)
+            uni.save()
 
     Connector.disconnect_database()
