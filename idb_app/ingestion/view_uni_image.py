0b61e5d44e90945c4fb9170613ee86fe9f112811
Silas Strawn <strawnsc@gmail.com>
2020-10-26 13:35:46 -0500

    allow viewing multiple images

--- idb_app/ingestion/view_uni_image.py
+++ idb_app/ingestion/view_uni_image.py
@@ -1,23 +1,27 @@
-import sys
 from PIL import Image
+import matplotlib.pyplot as plt
 
-from idb_app.models import University, UniversityImage
+from idb_app.models import UniversityImage
 from idb_app.mongo import Connector
+
+# standalone script to quickly cycle through university images
+# Beware using this script with a lot of images. You can't kill the process (even with ctrl+C) once started
 
 Connector.load_database_creds()
 
 Connector.connect_prod_database()
 
-# change this name to view another university image
-university_name = "The University of Texas Health Science Center at Houston"
-uni = University.objects(school_name=university_name).first()
-assert uni is not None
+query_set = UniversityImage.objects().limit(10)
 
-uni_image = UniversityImage.objects(university=uni).first()
-assert uni_image is not None
+fig, ax = plt.subplots()
 
-image = Image.open(uni_image.image)
-image.show()
+time_delay_per_image = 0.1
+for uni_image in query_set:
+    plt.cla()
+    plt.imshow(Image.open(uni_image.image))
+    ax.set_title(f"{uni_image.university.school_name}")
+    # Note that using time.sleep does *not* work here!
+    plt.pause(time_delay_per_image)
 
 
 Connector.disconnect_database()
