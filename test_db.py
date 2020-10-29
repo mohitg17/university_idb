6c5ec33cee5ed8c9ea6fd2a96a0f3ab76105f503
mohitg17 <mohit.gupta2563@gmail.com>
2020-10-28 23:31:35 -0500

    add image test

--- test_db.py
+++ test_db.py
@@ -19,16 +19,25 @@ class TestDB(unittest.TestCase):
         self.assertEqual(self.uni.id, ObjectId("5f965e1b81739c6287e0ccf8"))
         self.uni = University.objects(school_name="Harvard University", school_state="Massachusetts").first()
         self.assertEqual(self.uni.id, ObjectId("5f965e1b81739c6287e0ccf8"))
+        self.uni = University.objects(school_name="Harvard University", school_state="Massachusetts", 
+                                      school_city=City.objects(name="Cambridge").first().id).first()
+        self.assertEqual(self.uni.id, ObjectId("5f965e1b81739c6287e0ccf8"))
+        self.nothing = University.objects(school_name="UT Austin").first()
+        self.assertEqual(self.nothing, None)
 
     def test_city_query(self):
         self.city = City.objects(name="Austin").first()
         self.assertEqual(self.city.id, ObjectId("5f964eb5e2830ac6278784b3"))
         self.city = City.objects(name="Austin", state="Texas").first()
         self.assertEqual(self.city.id, ObjectId("5f964eb5e2830ac6278784b3"))
+        self.nothing = City.objects(name="austin").first()
+        self.assertEqual(self.nothing, None)
 
     def test_major_query(self):
         self.major = Major.objects(name="education").first()
         self.assertEqual(self.major.id, ObjectId('5f963df0608c791e121cacc7'))
+        self.nothing = Major.objects(name="edu").first()
+        self.assertEqual(self.nothing, None)
 
     def test_uni_image_query(self):
         self.image = UniversityImage.objects(university=ObjectId('5f965e1b81739c6287e0ccf8')).first()
@@ -40,7 +49,11 @@ class TestDB(unittest.TestCase):
 
     def test_major_ref(self):
         self.uni = University.objects(school_name="Harvard University").first()
-        self.assertEqual(self.uni.majors_offered[0], Major.objects(id=self.uni.majors_offered[0].id).first())
+        self.assertEqual(self.uni.majors_offered[0], Major.objects(id=self.uni.majors_offered[0].id).first())        
+
+    def test_uni_image(self):
+        self.image = UniversityImage.objects(university=ObjectId("5f965e1b81739c6287e0ccf8")).first()
+        self.assertEqual(self.image.id, ObjectId("5f972c43c2f60552d0590828"))
 
 if __name__ == '__main__':
     unittest.main()
